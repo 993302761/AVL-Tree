a02822aa1bcfd9c7a328968707bcde5a7d7de33e
雷宇琪 <993302761@qq.com>
2022-10-10 23:33:12 +0800

    二叉查找树已完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 #include "stdio.h"
 
-template<class T>
+template<class T>       //类模板
 
 struct treeNode{
     struct treeNode *parent;
@@ -18,15 +19,86 @@ struct treeNode{
     ef(0){}
 };
 
-struct node{
-    int a;
+template<class K>
+
+class AvlTree{
+
+private:
+
+    typedef treeNode<K> node;
+
+    node *root;
+
+    void freeNode(node *s){
+        if (s== nullptr)
+            return;
+        else{
+            freeNode(s->left);
+            freeNode(s->right);
+            delete s;
+        }
+    }
+
+
+    void getNode(node *n){
+        if (n== nullptr)
+            return;
+        getNode(n->left);
+        cout<<n->data<<endl;
+        getNode(n->right);
+    }
+
+public:
+
+    AvlTree():root(nullptr){}
+
+    ~AvlTree(){
+        freeNode(root);
+    }
+
+    bool insert(const K& data){
+        if(root== nullptr){
+            root=new node(data);
+        } else{
+            node *head=root;
+            node *s=new node(data);
+            while (head!= nullptr){
+                if (s->data<head->data){
+                    if (head->left!= nullptr){
+                        head= head->left;
+                    } else{
+                        head->left=s;
+                        s->parent=head;
+                        break;
+                    }
+                } else{
+                    if (head->right!= nullptr) {
+                        head = head->right;
+                    } else{
+                        head->right=s;
+                        s->parent=head;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void showTree(){
+        getNode(root);
+    }
+
 };
 
 
 int main() {
-//    struct treeNode<int> s= *(struct treeNode<int> *) malloc(sizeof (struct treeNode<int>));
-//    cout<<s.ef;
-
-    node s{1};
-    cout<<s.a;
+    AvlTree<int> s;
+    s.insert(6);
+    s.insert(3);
+    s.insert(2);
+    s.insert(5);
+    s.insert(8);
+    s.insert(7);
+    s.insert(9);
+    s.showTree();
 }

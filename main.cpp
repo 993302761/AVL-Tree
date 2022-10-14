0798e156b16a8a735e89a9b69866b4a358fbbd97
雷宇琪 <993302761@qq.com>
2022-10-14 11:15:54 +0800

    AVL树完成

--- main.cpp
+++ main.cpp
@@ -43,8 +43,8 @@ private:
     void getNode(node *n){
         if (n== nullptr)
             return;
-        getNode(n->left);
         cout<<n->data<<endl;
+        getNode(n->left);
         getNode(n->right);
     }
 
@@ -62,12 +62,20 @@ private:
         if ((parent->bf)==1||(parent->bf)==-1){
             updateBF(parent);
         } else if ((parent->bf)==2||(parent->bf)==-2){
-            check();
+            check(parent);
         }
     }
 
-    void check() {
-
+    void check(node *p) {
+        if (p->bf==-2&&p->left->bf==-1){
+            RR(p);
+        } else if (p->bf==2&&p->right->bf==1){
+            LL(p);
+        } else if (p->bf==-2&&p->left->bf==1){
+            LR(p);
+        } else if (p->bf==2&&p->right->bf==-1){
+            RL(p);
+        }
     }
 
     //左旋
@@ -76,14 +84,14 @@ private:
 
         //   让 right 的左子树 成为 p 的右子树
         p->right= right->left;
-        right->left->parent=p;
+        if (right->left!= nullptr){
+            right->left->parent=p;
+        }
 
         // 让 p 成为 right 的左子树
         if (p==root){
             root==right;
             right->parent= nullptr;
-            right->left=p;
-            p->parent=right;
         } else{
             if (p->parent->left==p){
                 p->parent->left=right;
@@ -91,9 +99,11 @@ private:
                 p->parent->right=right;
             }
             right->parent=p->parent;
-            right->left=p;
-            p->parent=right;
         }
+
+        right->left=p;
+        p->parent=right;
+
         p->bf=0;
         right->bf=0;
     }
@@ -104,14 +114,14 @@ private:
 
         //  让 left 的右子树 成为 p 的左子树
         p->left=left->right;
-        left->right->parent=p;
+        if (left->right!= nullptr){
+            left->right->parent=p;
+        }
 
         //        让 p 成为 left 的右子树
         if (p== root){
             root=left;
             left->parent= nullptr;
-            left->right=p;
-            p->parent=left;
         } else{
             if (p->parent->left==p){
                 p->parent->left=left;
@@ -119,23 +129,99 @@ private:
                 p->parent->right=left;
             }
             left->parent=p->parent;
-            left->right=p;
-            p->parent=left;
         }
+
+        left->right=p;
+        p->parent=left;
+
         p->bf=0;
         left->bf=0;
     }
 
 
     //左旋后右旋
-    void LR(){
-
+    void LR(node *p){
+        node *left=p->left;
+        node *leftR=left->right;
+        int bf=leftR->bf;
+        LL(p->left);
+        RR(p);
+        if (bf==1){
+            p->bf=1;
+            left->bf=0;
+            leftR->bf=0;
+        } else if (bf==-1){
+            p->bf=0;
+            left->bf=-1;
+            leftR->bf=0;
+        } else if (bf==0){
+            p->bf=0;
+            left->bf=0;
+            leftR->bf=0;
+        }
     }
 
     //右旋后左旋
-    void RL(){
-
+    void RL(node *p){
+        node *right=p->right;
+        node *rightL=right->left;
+        int bf=rightL->bf;
+        RR(p->right);
+        LL(p);
+        if (bf==1){
+            p->bf=-1;
+            right->bf=0;
+            rightL->bf=0;
+        } else if (bf==-1){
+            p->bf=0;
+            right->bf=1;
+            rightL->bf=0;
+        } else if (bf==0){
+            p->bf=0;
+            right->bf=0;
+            rightL->bf=0;
+        }
     }
+
+    int Height(node *p){
+        if (p== nullptr){
+            return 0;
+        }
+        int left=Height(p->left);
+        int right=Height(p->right);
+
+        return max(left,right)+1;
+    }
+
+
+
+    int IsBalance2(node * root)
+    {
+        // 先判断当前树的左、右子树是否平衡，再判断当前树是否平衡
+        // 不平衡返回-1，平衡则返回当前树的高度
+
+        // 当前树为空，返回高度0
+        if (root == nullptr)
+            return 0;
+
+        // 当前树不为空，分别计算左右子树的高度
+        int leftHeight = IsBalance2(root->left);
+        int rightHeight = IsBalance2(root->right);
+
+        if (rightHeight - leftHeight != root->bf) // 检查当前树的平衡因子是否计算正确
+        {
+            cout << "平衡因子异常:" << root->data << endl;
+        }
+
+        // 左子树高度等于-1、右子树高度等于-1、左右子树高度差的绝对值大于1，说明当前树不平衡
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1)
+            return -1;
+
+        // 运行到这里来了，说明当前树是平衡的，返回当前树的高度
+        return max(leftHeight, rightHeight) + 1;
+    }
+
+
 public:
 
     AvlTree():root(nullptr){}
@@ -194,6 +280,18 @@ public:
         getNode(root);
     }
 
+    int getHeight(){
+        return Height(root);
+    }
+
+
+    bool IsBalance()
+    {
+        return IsBalance2(root) != -1;
+    }
+
+
+
 };
 
 
@@ -201,10 +299,15 @@ int main() {
     AvlTree<int> s;
     s.insert(6);
     s.insert(3);
+    s.insert(8);
     s.insert(2);
     s.insert(5);
-    s.insert(8);
+    s.insert(1);
     s.insert(7);
     s.insert(9);
     s.showTree();
+    cout<<"---------------------"<<endl;
+    cout<<s.getHeight()<<endl;
+    cout<<"---------------------"<<endl;
+    cout<<s.IsBalance()<<endl;
 }
